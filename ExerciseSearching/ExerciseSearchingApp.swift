72e5406d79ca5c066e2e4f0f0f06a04a9e0a6454
shenqing <shenqin1@msu.edu>
2023-05-19 13:49:21 -0400

    Initial Commit

---
+++ ExerciseSearching/ExerciseSearchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseSearchingApp.swift
+//  ExerciseSearching
+//
+//  Created by 沈清昊 on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseSearchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
